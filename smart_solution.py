7f83122f9b6f0ff3c265da41e76937f4a119f2a9
Andrii Semchenko <a7chenko@gmail.com>
2020-03-07 23:13:34 +0200

    [РЕШЕНО] Решение - пушка-ракета

--- smart_solution.py
+++ smart_solution.py
@@ -9,37 +9,36 @@ def get_start_value(number):
 
 
 def find(little_exp, big_exp, number):
-    number_backup = number
+    if little_exp == 0:
+        return None
 
     cur_little = (number // little_exp) % 10
     cur_big = (number // big_exp) % 10
-    print(cur_little, ':', cur_big)
     while cur_big < 9 and cur_little > 1:
         cur_big += 1
         cur_little -= 1
         # updating number
         number = number - little_exp + big_exp
-        print(number)
         # checking if number match conditions
         if number % N == 0:
             return number
-    # if no success
-    number = number_backup
-    cur_little = (number // little_exp) % 10
-    cur_big = (number // big_exp) % 10
-    print(cur_little, ':', cur_big)
-    while cur_big != 9 and cur_little != 1:
-        cur_big += 1
-        cur_little -= 1
-        # updating number
-        number = number - little_exp + big_exp
-        print(number)
-        # checking if number match conditions
-        return find(little_exp // 10, big_exp, number)
+        res = find(little_exp // 10, big_exp // 10, number)
+        if res is None:
+            continue
+        return res
 
 
 N = int(argv[1])
 cur = get_start_value(N)
-b_exp = int(10 ** (len(str(cur)) - 1))
-l_exp = int(b_exp / 10)
-print(find(l_exp, b_exp, cur))
+
+
+def find_result(start_value):
+    b_exp = int(10 ** (len(str(start_value)) - 1))
+    l_exp = int(b_exp / 10)
+    return find(l_exp, b_exp, start_value)
+
+
+r = find_result(cur)
+if r is None:
+    r = find_result(cur * 10)
+print(r)
