bbaaae5f865e730253cf4880bcb1f5697bbc092c
Andrii Semchenko <a7chenko@gmail.com>
2020-03-07 22:54:03 +0200

    Just another strange commit

--- smart_solution.py
+++ smart_solution.py
@@ -13,11 +13,13 @@ def find(little_exp, big_exp, number):
 
     cur_little = (number // little_exp) % 10
     cur_big = (number // big_exp) % 10
-    while cur_big != 9:
+    print(cur_little, ':', cur_big)
+    while cur_big < 9 and cur_little > 1:
         cur_big += 1
         cur_little -= 1
         # updating number
         number = number - little_exp + big_exp
+        print(number)
         # checking if number match conditions
         if number % N == 0:
             return number
@@ -25,8 +27,19 @@ def find(little_exp, big_exp, number):
     number = number_backup
     cur_little = (number // little_exp) % 10
     cur_big = (number // big_exp) % 10
-
+    print(cur_little, ':', cur_big)
+    while cur_big != 9 and cur_little != 1:
+        cur_big += 1
+        cur_little -= 1
+        # updating number
+        number = number - little_exp + big_exp
+        print(number)
+        # checking if number match conditions
+        return find(little_exp // 10, big_exp, number)
 
 
 N = int(argv[1])
 cur = get_start_value(N)
+b_exp = int(10 ** (len(str(cur)) - 1))
+l_exp = int(b_exp / 10)
+print(find(l_exp, b_exp, cur))
